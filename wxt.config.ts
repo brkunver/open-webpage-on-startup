f4579bde01d94906b7b2eefc9c66659137360842
burak unver <burakhanunver@gmail.com>
2025-10-30 16:40:45 +0300

    add i18n package

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,9 +3,10 @@ import tailwindcss from "@tailwindcss/vite"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-solid"],
+  modules: ["@wxt-dev/module-solid", "@wxt-dev/i18n/module"],
   manifest: {
     default_locale: "en",
+    name: "__MSG_extensionName__",
   },
   vite: () => ({
     plugins: [tailwindcss()],
