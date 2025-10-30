d32345a04985243f631995a3a7202ee7280d5c90
burak unver <burakhanunver@gmail.com>
2025-10-30 16:43:28 +0300

    add basic tw styles

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -4,8 +4,8 @@ import { i18n } from "#i18n"
 export default function App() {
   const t = i18n.t
   return (
-    <>
-      <h1>{t("extensionName")}</h1>
-    </>
+    <div class="flex flex-col min-w-[400px] min-h-[200px]">
+      <h1 class="text-2xl text-center font-bold">{t("extensionName")}</h1>
+    </div>
   )
 }
