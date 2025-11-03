2383ccad7cf5d0d09bd7a60ece8175142fd0095b
burak unver <burakhanunver@gmail.com>
2025-11-03 15:09:31 +0300

    add freq selector

--- components/add-new-webpage.svelte
+++ components/add-new-webpage.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
   import { i18n } from "#i18n"
   import { webpageStorage } from "@/utils/storage"
+  import Select from "@/components/select.svelte"
 
-  let url = $state("")
-  let name = $state("")
+  let selected = $state({ label: "Everytime", value: "everytime" })
+  const options = [
+    {
+      label: "Everytime",
+      value: "everytime",
+    },
+    {
+      label: "Daily Once",
+      value: "daily_once",
+    },
+    {
+      label: "Passive",
+      value: "passive",
+    },
+  ]
+  let url = $state<string>("")
+  let name = $state<string>("")
 
   const t = i18n.t
 
@@ -12,7 +28,7 @@
     let newPage: Webpage = {
       name: name,
       url: url,
-      repeat: "everytime",
+      repeat: selected.value as "everytime" | "daily_once" | "passive",
       lastOpened: new Date().toISOString(),
     }
 
@@ -26,6 +42,7 @@
   <h1 class="text-xl font-bold">{t("addNewWebpage")}</h1>
   <input type="text" placeholder="Name" bind:value={name} />
   <input type="text" placeholder="URL" bind:value={url} />
+  <Select {options} bind:value={selected} placeholder="Select an option" />
   <button class="cursor-pointer bg-blue-800 text-white p-2 rounded w-fit" onclick={addNewWebpage}>
     {t("addNewWebpage")}
   </button>
