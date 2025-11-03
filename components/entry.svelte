0b15c288d45e52d1fc261dced4aea39e9df81d6a
burak unver <burakhanunver@gmail.com>
2025-11-03 14:33:42 +0300

    add transition:fade

--- components/entry.svelte
+++ components/entry.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { Webpage } from "@/utils/storage"
   import { webpageStorage } from "@/utils/storage"
+  import { fade } from "svelte/transition"
 
   export let webpage: Webpage
 
@@ -11,12 +12,10 @@
   }
 </script>
 
-<div class="flex flex-col gap-1 p-1 m-1">
+<div class="flex flex-col gap-1 p-1 m-1" transition:fade>
   <p>{webpage.name}</p>
   <p>{webpage.url}</p>
   <p>{webpage.repeat}</p>
   <p>{webpage.lastOpened}</p>
-  <button class="cursor-pointer bg-red-800 text-white p-2 rounded w-fit" on:click={deleteWebpage}>
-    Delete
-  </button>
+  <button class="cursor-pointer bg-red-800 text-white p-2 rounded w-fit" onclick={deleteWebpage}> Delete </button>
 </div>
