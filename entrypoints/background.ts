ff9d90ca1cbbb38eb0f258a82796bff4b5d738e0
burak unver <burakhanunver@gmail.com>
2025-11-04 15:27:39 +0300

    add opener logic

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,29 @@
+import { webpageStorage } from "@/utils/storage"
+
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
-});
+  browser.runtime.onStartup.addListener(async () => {
+    const now = new Date()
+    const sites = await webpageStorage.getValue()
+    const toOpen: Webpage[] = []
+
+    // testing opening tabs
+    // await browser.tabs.create({ url: "https://wxt.dev" })
+
+    for (const site of sites) {
+      if (checkCondition(site, now)) {
+        toOpen.push(site)
+      }
+    }
+
+    // Open tabs
+    for (const site of toOpen) {
+      await browser.tabs.create({ url: site.url })
+      // Update lastOpened
+      site.lastOpened = now.getTime()
+    }
+
+    // Update Storage
+    await webpageStorage.setValue(sites)
+    console.log(`Startup tabs: ${toOpen.length} tabs opened.`)
+  })
+})
